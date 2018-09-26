22dbe7396fcf280e60e55a5c1cc3f7e1d3ceea00
suransea <simpleslight@outlook.com>
2018-09-26 10:34:17 +0800

    feat: 加入数据源连接方式

--- jdbc/src/com/sea/lyra/jdbc/LyraDataSource.java
+++ jdbc/src/com/sea/lyra/jdbc/LyraDataSource.java
@@ -3,16 +3,22 @@ package com.sea.lyra.jdbc;
 import javax.sql.DataSource;
 import java.io.PrintWriter;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.SQLFeatureNotSupportedException;
+import java.util.Stack;
 import java.util.logging.Logger;
 
 public class LyraDataSource implements DataSource {
-    private String url;
-    private String user;
-    private String password;
-    private String driverClassName;
+    private String url = "";
+    private String user = "";
+    private String password = "";
+    private String driverClassName = "";
+
+    public LyraDataSource() {
+    }
+
+    private static Stack<LyraConnectionWrapper> pool = new Stack<>();
+    private static Stack<String> urls = new Stack<>();
 
     public String getUrl() {
         return url;
@@ -48,22 +54,28 @@ public class LyraDataSource implements DataSource {
 
     @Override
     public Connection getConnection() throws SQLException {
-        try {
-            Class.forName(driverClassName);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return DriverManager.getConnection(url, user, password);
+        return getConnection(user, password);
     }
 
     @Override
     public Connection getConnection(String username, String password) throws SQLException {
-        try {
-            Class.forName(driverClassName);
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (!pool.empty() && urls.peek().equals(url)) {
+            urls.pop();
+            return pool.pop();
         }
-        return DriverManager.getConnection(url, username, password);
+        ConnectionBuilder connectionBuilder = new LyraConnectionBuilder(url);
+        Connection connection = connectionBuilder
+                .user(username)
+                .password(password)
+                .build();
+        return new LyraConnectionWrapper(connection, x -> {
+            pool.push(x);
+            urls.push(url);
+        });
+    }
+
+    public interface CloseAction {
+        void act(LyraConnectionWrapper wrapper);
     }
 
     @Override
--- jdbc/src/test.java
+++ jdbc/src/test.java
@@ -1,45 +1,59 @@
-import java.sql.*;
-import java.util.Random;
+import com.sea.lyra.jdbc.LyraDataSource;
+
+import java.sql.Connection;
+import java.sql.Statement;
 
 public class test {
     public static void main(String[] args) {
         try {
-            Class.forName("com.sea.lyra.Driver");
-            Connection connection = DriverManager
-                    .getConnection("jdbc:lyra://localhost:5494/test",
-                            "root",
-                            "123456");
-            System.out.println(connection);
+//            Class.forName("com.sea.lyra.Driver");
+//            Connection connection = DriverManager
+//                    .getConnection("jdbc:lyra://localhost:5494/test",
+//                            "root",
+//                            "123456");
+//            System.out.println(connection);
+//            Statement statement = connection.createStatement();
+//            statement.execute("create table test_table (id int,a int ,b varchar(100),c int , d varchar(10))");
+//            StringBuilder sql = new StringBuilder("insert into test_table values");
+//            Random random = new Random();
+//            for (int i = 0; i < 1000000; i++) {
+//                sql.append(String.format("(%d,%d,'%d',%d,'%d'),",
+//                        i,
+//                        random.nextInt(),
+//                        random.nextInt(),
+//                        random.nextInt(),
+//                        random.nextInt()
+//                ));
+//            }
+//            sql.append("(1000000,213812,'end',214325,'wuuewu')");
+//            long time = System.currentTimeMillis();
+//            System.out.println("begin");
+//            statement.execute(sql.toString());
+//            System.out.println(System.currentTimeMillis() - time);
+//            ResultSet resultSet = statement.executeQuery("select * from test_table");
+//            System.out.println(resultSet.getRow());//获取行数
+//            while (resultSet.next()) {
+//                ResultSetMetaData metaData = resultSet.getMetaData();
+//                int columnCount = metaData.getColumnCount();// 获取列数
+//                for (int i = 1; i <= columnCount; i++) {
+//                    String columnName = metaData.getColumnName(i);//根据索引获取列名
+//                    String data = resultSet.getString(columnName);
+//                    System.out.println(data);
+//                }
+//            }
+//            statement.close();
+//            connection.close();
+            LyraDataSource dataSource = new LyraDataSource();
+            dataSource.setUrl("jdbc:lyra://localhost:5494/test");
+            Connection connection = dataSource.getConnection("root", "123456");
             Statement statement = connection.createStatement();
-            statement.execute("create table test_table (id int,a int ,b varchar(100),c int , d varchar(10))");
-            StringBuilder sql = new StringBuilder("insert into test_table values");
-            Random random = new Random();
-            for (int i = 0; i < 1000000; i++) {
-                sql.append(String.format("(%d,%d,'%d',%d,'%d'),",
-                        i,
-                        random.nextInt(),
-                        random.nextInt(),
-                        random.nextInt(),
-                        random.nextInt()
-                ));
-            }
-            sql.append("(1000000,213812,'end',214325,'wuuewu')");
-            long time = System.currentTimeMillis();
-            System.out.println("begin");
-            statement.execute(sql.toString());
-            System.out.println(System.currentTimeMillis() - time);
-            ResultSet resultSet = statement.executeQuery("select * from test_table");
-            System.out.println(resultSet.getRow());//获取行数
-            while (resultSet.next()) {
-                ResultSetMetaData metaData = resultSet.getMetaData();
-                int columnCount = metaData.getColumnCount();// 获取列数
-                for (int i = 1; i <= columnCount; i++) {
-                    String columnName = metaData.getColumnName(i);//根据索引获取列名
-                    String data = resultSet.getString(columnName);
-                    System.out.println(data);
-                }
-            }
-            statement.close();
+            System.out.println(statement.execute("show databases"));
+            connection.close();
+            dataSource = new LyraDataSource();
+            dataSource.setUrl("jdbc:lyra://localhost:5494/lyra");
+            connection = dataSource.getConnection("root", "123456");
+            statement = connection.createStatement();
+            System.out.println(statement.execute("show databases"));
             connection.close();
         } catch (Exception e) {
             e.printStackTrace();
